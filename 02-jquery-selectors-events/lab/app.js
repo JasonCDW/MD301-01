6c143e83aecfcf2314ae4ea909cb2ed13cf1d802
JasonCDW <dw.jay.12@gmail.com>
2018-10-11 21:04:03 -0400

    lab02

---
+++ 02-jquery-selectors-events/lab/app.js
@@ -0,0 +1,38 @@
+const $main = $('main')
+const $selector = $('#filter')
+
+const apiURL = 'https://raw.githubusercontent.com/CodePartnersMD/MD301-01/master/02-jquery-selectors-events/lab/page-1.json'
+
+const HornImg = function(imageObj) {
+  this.title = imageObj.title;
+  this.image_url = imageObj.image_url;
+  this.description = imageObj.description;
+  this.keyword = imageObj.keyword;
+  this.horns = imageObj.horns;
+}
+
+let HornsArray = [];
+
+HornImg.prototype.renderHorns = imgObj => {
+  let $HornClone = $('#image-template').clone()
+  $main.append($HornClone)
+  $HornClone.attr('id', imgObj.keyword)
+  $HornClone.find('img').attr('src', imgObj.image_url)
+  $HornClone.find('p').text(imgObj.description)
+  $HornClone.find('h2').text(imgObj.title)
+}
+
+$($selector).on('change', () => {
+  $('section').hide()
+  $(`section[id=${event.target.value}]`).show()
+})
+
+$.getJSON(apiURL, response => {
+  response.forEach((val) => {
+    let newHorn = new HornImg(val)
+    HornsArray.push(newHorn)
+    newHorn.renderHorns(val)
+    $selector.append(`<option value=${newHorn.keyword}>${newHorn.keyword}</option>`)
+  })
+})
+
